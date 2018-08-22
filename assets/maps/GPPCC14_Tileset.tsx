cd0cd918671a336a666ce089d0bad23383270a08
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2018-08-22 16:58:18 +0200

    Added my own tileset

---
+++ assets/maps/GPPCC14_Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="GPPCC14_Tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="72" columns="12">
+ <image source="GPPCC14_Tileset.png" width="203" height="101"/>
+</tileset>
