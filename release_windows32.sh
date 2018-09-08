e448ead1c61049f91b3064aa709c75d45249282a
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2018-09-08 22:00:33 +0200

    Added the dlls to the zip

--- release_windows32.sh
+++ release_windows32.sh
@@ -2,6 +2,6 @@ echo Compiling ...
 go build -a -ldflags '-s' -o GPPCC14_x86.exe
 echo Compiling done!
 echo Packing files of GPPCC14 ...
-zip GPPCC14_x86.zip GPPCC14_x86.exe assets/fonts/* assets/maps/* assets/textures/*
+zip GPPCC14_x86.zip GPPCC14_x86.exe assets/fonts/* assets/maps/* assets/textures/* /mingw32/bin/libopenal-1.dll
 echo Created GPPCC14_x86.zip
 echo Done!
