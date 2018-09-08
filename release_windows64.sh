22660fc5ac21cf2af9672d22ab929b1f9e0c3b96
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2018-09-08 22:35:01 +0200

    Fixed directory of libopenal-1.dll

--- release_windows64.sh
+++ release_windows64.sh
@@ -2,6 +2,8 @@ echo Compiling ...
 go build -a -ldflags '-s' -o GPPCC14_x64.exe
 echo Compiling done!
 echo Packing files of GPPCC14 ...
-zip GPPCC14_x64.zip GPPCC14_x64.exe assets/fonts/* assets/maps/* assets/textures/* /mingw64/bin/libopenal-1.dll
+cp /mingw64/bin/libopenal-1.dll libopenal-1.dll
+zip GPPCC14_x64.zip GPPCC14_x64.exe assets/fonts/* assets/maps/* assets/textures/* libopenal-1.dll
+rm libopenal-1.dll
 echo Created GPPCC14_x64.zip
 echo Done!
