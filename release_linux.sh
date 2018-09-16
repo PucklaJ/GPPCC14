991ee19530d121c18c5fcdc13305a7a6a700ca05
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2018-09-16 14:31:14 +0200

    Added sounds to release scripts

--- release_linux.sh
+++ release_linux.sh
@@ -2,6 +2,6 @@ echo Compiling ...
 go build -a -ldflags '-s' -o GPPCC14
 echo Compiling done!
 echo Packing files of GPPCC14 ...
-tar -cf GPPCC14.tar GPPCC14 assets/fonts/* assets/maps/* assets/textures/*
+tar -cf GPPCC14.tar GPPCC14 assets/fonts/* assets/maps/* assets/textures/* assets/sounds/*
 echo Created GPPCC14.tar
 echo Done!
